e1c2eb34e3cabea41314494330ce4b078f3c7d7b
felipeleal27 <felipe.leal@aluno.unifenas.br>
2024-09-07 00:44:23 -0300

    .

--- FrontEnd/gestao_financeira/lib/app/viewmmodel/login/login_viewmodel.g.dart
+++ FrontEnd/gestao_financeira/lib/app/viewmmodel/login/login_viewmodel.g.dart
@@ -8,11 +8,27 @@ part of 'login_viewmodel.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
-mixin _$LoginViewmodel on LoginViewmodelBase, Store {
+mixin _$LoginViewModel on LoginViewmodelBase, Store {
+  late final _$chengeObscureTextAtom =
+      Atom(name: 'LoginViewmodelBase.chengeObscureText', context: context);
+
+  @override
+  bool get chengeObscureText {
+    _$chengeObscureTextAtom.reportRead();
+    return super.chengeObscureText;
+  }
+
+  @override
+  set chengeObscureText(bool value) {
+    _$chengeObscureTextAtom.reportWrite(value, super.chengeObscureText, () {
+      super.chengeObscureText = value;
+    });
+  }
+
   @override
   String toString() {
     return '''
-
+chengeObscureText: ${chengeObscureText}
     ''';
   }
 }
